8c70f7602f7a237b0f82166e5e48a7c5627edb2f
Matheus Neto <matheusneto23@gmail.com>
2021-09-02 14:51:39 -0300

    [WIP] Fix generator and improve gitignore

--- TL_GAN.py
+++ TL_GAN.py
@@ -9,12 +9,12 @@ from numpy import ones
 from numpy import zeros
 from numpy.random import randint
 from numpy.random import randn
-from tensorflow.keras import Sequential, Model, Input
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Concatenate, Dense, BatchNormalization, \
-    LeakyReLU, Reshape, Flatten
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, BatchNormalization, \
+    LeakyReLU, Reshape, Flatten, Conv2DTranspose
 from tensorflow.keras.layers.experimental.preprocessing import Resizing
-from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import load_img
 
 from utils.gpu import set_gpu_limit
 
@@ -51,41 +51,25 @@ def get_discriminator(input_size=(250, 450, 3)):
 
 
 def get_generator(input_dim=100):
-    gen_input = Input(input_dim)
-    init = LeakyReLU()(BatchNormalization()(Dense(16 * 29 * 1024, input_dim=input_dim, use_bias=False)(gen_input)))
-    init = Reshape((16, 29, 1024))(init)
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(init)
-
-    up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
-        UpSampling2D(size=(2, 2))(conv5))
-    merge6 = Concatenate(axis=3)([up6])
-    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
-
-    up1 = UpSampling2D(size=(2, 2))(conv6)
-    up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up1)
-    conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(up1)
-    merge7 = Concatenate(axis=3)([conv3, up7])
-    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
-
-    up2 = UpSampling2D(size=(2, 2))(conv7)
-    up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up2)
-    conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(up2)
-    merge8 = Concatenate(axis=3)([conv2, up8])
-    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
-
-    up3 = UpSampling2D(size=(2, 2))(conv8)
-    up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(up3)
-    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(up3)
-    merge9 = Concatenate(axis=3)([conv1, up9])
-    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    conv9 = Conv2D(3, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-    output = Resizing(height=250, width=450)(conv9)
-    model = Model(inputs=[gen_input], outputs=[output])
-
+    model = Sequential()
+    model.add(Dense(16 * 29 * 1024, input_dim=input_dim, use_bias=False))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    model.add(Reshape((16, 29, 1024)))
+    model.add(Conv2D(1024, (3, 3), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2DTranspose(512, (2, 2), strides=(2, 2), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2DTranspose(256, (2, 2), strides=(2, 2), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2DTranspose(128, (2, 2), strides=(2, 2), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Conv2DTranspose(64, (2, 2), strides=(2, 2), activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Conv2D(3, 3, activation='relu', padding='same', kernel_initializer='he_normal'))
+    model.add(Resizing(height=250, width=450))
     model.compile(optimizer=Adam(learning_rate=3e-4), loss='binary_crossentropy', metrics=['accuracy'])
 
     return model
@@ -233,5 +217,6 @@ if __name__ == '__main__':
     gan = get_gan(discriminator, generator)
     gan.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=2e-4, beta_1=0.05))
 
-    dataset = load_real_samples(args.dataset_path)
-    train(generator, discriminator, gan, dataset, latent_dim)
+    # dataset = load_real_samples(args.dataset_path)
+    # train(generator, discriminator, gan, dataset, latent_dim)
+    generator.summary()
