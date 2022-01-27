535e7ac05b7eb3fe80fd6f9a74cc3764aac83eeb
Matheus Neto <matheusneto23@gmail.com>
2022-01-27 19:55:12 -0300

    trying create another models

--- dataset/dysplasia_dataset.py
+++ dataset/dysplasia_dataset.py
@@ -1,14 +1,13 @@
 # dataset settings
 dataset_type = 'DysplasiaDataSet'
 data_root = 'segmentation'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-img_scale = (320, 240)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# img_scale = (320, 240)
 crop_size = (256, 256)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(320, 240), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -21,7 +20,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=img_scale,
+        img_scale=(320, 240),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
