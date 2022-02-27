a8715e850ea42960877a71489709a6eaeeb855cd
Matheus Neto <matheusneto23@gmail.com>
2022-02-27 14:22:49 -0300

    Changing the way to save the results.

--- utils/dysplasia_dataset.py
+++ utils/dysplasia_dataset.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'DysplasiaDataSet'
-data_root = 'segmentation'
+data_root = 'ToTrain'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 # img_scale = (320, 240)
 crop_size = (256, 256)
