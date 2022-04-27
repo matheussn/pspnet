5098793dad1fca989d0732f09cd39a1f4880e16f
Matheus Neto <matheusneto23@gmail.com>
2022-04-27 18:07:41 -0300

    Create notebook to show dataAug and fix data_aug.m

--- data_aug/data_aug.m
+++ data_aug/data_aug.m
@@ -7,9 +7,13 @@ mkdir Aug/annotations
 mkdir Aug/images/healthy
 mkdir Aug/annotations/healthy
 
+
+
+
 input_width = 400;
 input_height = 192;
 target = ["healthy"];
+
 for class = 1 : length(target)
     fprintf('\t%s\n', target(class));
     images = imageDatastore(strcat('dataset/', target(class)), 'IncludeSubfolders', true);
@@ -28,7 +32,7 @@ for class = 1 : length(target)
 
         imwrite(image, strcat("./Aug/images/", target(class), "/", img_name, ".tif"))
         imwrite(ground, strcat("./Aug/annotations/", target(class), "/", img_name, ".tif"))
-        
+
         fprintf(1, '\t\tNow reading %d of %d\n', currImage, length(images.Files));
         original_center_img = center_crop(image, input_width, input_height, 0, 0);
         original_center_ground = center_crop(ground, input_width, input_height, 0, 0);
@@ -41,7 +45,7 @@ for class = 1 : length(target)
 
         imwrite(img_noise, strcat("./Aug/images/", target(class), "/gaussian_", img_name, ".tif"))
         imwrite(original_center_ground, strcat("./Aug/annotations/", target(class), "/gaussian_", img_name, ".tif"))
-        
+
         img_noise_salt_and_pepper = imnoise(original_center_img,'salt & pepper',0.04);
         % ground_noise_salt_and_pepper = imnoise(original_center_img,'salt & pepper',0.04);
 
@@ -56,7 +60,7 @@ for class = 1 : length(target)
 
         img_rotated = imrotate(original_center_img, 5);
         ground_rotated = imrotate(original_center_ground, 5);
-        
+
         imwrite(img_rotated, strcat("./Aug/images/", target(class), "/rotated_", img_name, ".tif"))
         imwrite(ground_rotated, strcat("./Aug/annotations/", target(class), "/rotated_", img_name, ".tif"))
     end
