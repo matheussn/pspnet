700b8bd0243230829c40b8e96233860d5738a853
Matheus Neto <matheusneto23@gmail.com>
2022-07-28 21:04:01 -0300

    Improve names

--- data_aug/data_aug.m
+++ data_aug/data_aug.m
@@ -7,12 +7,11 @@ mkdir Aug/annotations
 mkdir Aug/images/healthy
 mkdir Aug/annotations/healthy
 
-
-
-
 input_width = 400;
 input_height = 192;
 target = ["healthy"];
+shifting_value = [100,    0, -100,    0;
+                    0   100,    0, -100];
 
 for class = 1 : length(target)
     fprintf('\t%s\n', target(class));
@@ -37,32 +36,40 @@ for class = 1 : length(target)
         original_center_img = center_crop(image, input_width, input_height, 0, 0);
         original_center_ground = center_crop(ground, input_width, input_height, 0, 0);
 
-        imwrite(original_center_img, strcat("./Aug/images/", target(class), "/center_", img_name, ".tif"))
-        imwrite(original_center_ground, strcat("./Aug/annotations/", target(class), "/center_", img_name, ".tif"))
+        imwrite(original_center_img, strcat("./Aug/images/", target(class), "/", img_name, "_center.tif"))
+        imwrite(original_center_ground, strcat("./Aug/annotations/", target(class), "/", img_name, "_center.tif"))
 
         img_noise = imnoise(original_center_img,'gaussian',0.04);
         % ground_noise = imnoise(original_center_ground,'gaussian',0.04);
 
-        imwrite(img_noise, strcat("./Aug/images/", target(class), "/gaussian_", img_name, ".tif"))
-        imwrite(original_center_ground, strcat("./Aug/annotations/", target(class), "/gaussian_", img_name, ".tif"))
+        imwrite(img_noise, strcat("./Aug/images/", target(class), "/", img_name, "_gaussian.tif"))
+        imwrite(original_center_ground, strcat("./Aug/annotations/", target(class), "/", img_name, "_gaussian.tif"))
 
         img_noise_salt_and_pepper = imnoise(original_center_img,'salt & pepper',0.04);
         % ground_noise_salt_and_pepper = imnoise(original_center_img,'salt & pepper',0.04);
 
-        imwrite(img_noise_salt_and_pepper, strcat("./Aug/images/", target(class), "/salt_and_pepper_", img_name, ".tif"))
-        imwrite(original_center_ground, strcat("./Aug/annotations/", target(class), "/salt_and_pepper_", img_name, ".tif"))
+        imwrite(img_noise_salt_and_pepper, strcat("./Aug/images/", target(class), "/", img_name, "_salt_and_pepper.tif"))
+        imwrite(original_center_ground, strcat("./Aug/annotations/", target(class), "/", img_name, "_salt_and_pepper.tif"))
 
         img_resized = imresize(image, [input_width input_height]);
         ground_resized = imresize(ground, [input_width input_height]);
 
-        imwrite(img_resized, strcat("./Aug/images/", target(class), "/resized_", img_name, ".tif"))
-        imwrite(ground_resized, strcat("./Aug/annotations/", target(class), "/resized_", img_name, ".tif"))
+        imwrite(img_resized, strcat("./Aug/images/", target(class), "/", img_name, "_resized.tif"))
+        imwrite(ground_resized, strcat("./Aug/annotations/", target(class), "/", img_name, "_resized.tif"))
 
         img_rotated = imrotate(original_center_img, 5);
         ground_rotated = imrotate(original_center_ground, 5);
 
-        imwrite(img_rotated, strcat("./Aug/images/", target(class), "/rotated_", img_name, ".tif"))
-        imwrite(ground_rotated, strcat("./Aug/annotations/", target(class), "/rotated_", img_name, ".tif"))
+        imwrite(img_rotated, strcat("./Aug/images/", target(class), "/", img_name, "_rotated.tif"))
+        imwrite(ground_rotated, strcat("./Aug/annotations/", target(class), "/", img_name, "_rotated.tif"))
+
+        for i=1:4
+          center_img = center_crop(image, input_width, input_height, shifting_value(1,i), shifting_value(2,i));
+          center_ground = center_crop(ground, input_width, input_height, shifting_value(1,i), shifting_value(2,1));
+
+          imwrite(center_img, strcat("./Aug/images/", target(class), "/", img_name, "_randon_", i, ".tif"));
+          imwrite(center_ground, strcat("./Aug/annotations/", target(class), "/", img_name, "_randon_", i, ".tif"));
+        end
     end
 end
 
@@ -74,3 +81,4 @@ function [center_img] = center_crop(img, width, height, shiftwidth, shiftheight 
     widthright = widthleft+width;
     center_img = img(heightbot:heighttop, widthleft:widthright,1:3);
 end
+
